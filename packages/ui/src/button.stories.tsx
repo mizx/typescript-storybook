ca11818398be328c9e8af5a5e7577535cdbcd6c2
Cody <mizx@users.noreply.github.com>
2018-06-06 23:38:09 -0700

    basic buttons with ts docgen working

--- packages/ui/src/button.stories.tsx
+++ packages/ui/src/button.stories.tsx
@@ -1,3 +1,4 @@
+import { withInfo } from '@storybook/addon-info';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
@@ -7,7 +8,23 @@ const stories = storiesOf('Buttons', module);
 
 stories.add(
     'Label',
-    () => (
-        <Button label="example label" />
+    withInfo({ inline: true })(
+        () => (
+            <Button
+                label="example label"
+            />
+        )
+    )
+)
+
+stories.add(
+    'Label with active class',
+    withInfo({ inline: true })(
+        () => (
+            <Button
+                label="example label"
+                active={true}
+            />
+        )
     )
 )
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, MouseEvent } from 'react';
 
 interface IProps {
+    /** apply active classname */
     active?: boolean;
+    /** label for button */
     label: string;
+    /** color to apply to button */
+    color?: 'blue' | 'green';
+    /**
+     * callback for when button is clicked
+     * note: this can still be called when disabled
+     */
+    onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
 export class Button extends Component<IProps> {
 
+    static defaultProps: Partial<IProps> = {
+        active: false,
+        color: 'blue'
+    }
+
     public render() {
         return (
             <button
+                onClick={this.props.onClick}
                 className={this.props.active ? 'active' : undefined}
             >
                 {this.props.label}
